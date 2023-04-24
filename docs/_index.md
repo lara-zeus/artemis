d0dda00c1451b16ad76e0ebcf3aed0783c74d9e2
Ash Monsh <wh7r.com@gmail.com>
2023-04-24 08:39:57 +0300

    🎨

---
+++ docs/_index.md
@@ -0,0 +1,7 @@
+---
+title: v1
+slogan: themes for Lara Zeus Sky.
+githubUrl: https://github.com/lara-zeus/artemis
+branch: main
+icon: ri-cloud-windy-line
+---
---
+++ src/ArtemisServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace LaraZeus\Artemis;
+
+use Filament\PluginServiceProvider;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\View;
+use LaraZeus\Sky\Console\migrateCommand;
+use LaraZeus\Sky\Filament\Resources\TagResource;
+use Spatie\LaravelPackageTools\Package;
+
+class ArtemisServiceProvider extends PluginServiceProvider
+{
+    // 🎨
+    public static string $name = 'zeus-sky';
+
+    public function boot()
+    {
+        View::share('theme', 'zeus-sky::themes.' . config('zeus-sky.theme', 'zeus'));
+
+        App::singleton('theme', function () {
+            return 'zeus-sky::themes.' . config('zeus-sky.theme', 'zeus');
+        });
+
+        return parent::boot();
+    }
+
+    public function configurePackage(Package $package): void
+    {
+        parent::configurePackage($package);
+        $package
+            ->hasConfigFile()
+            ->hasMigrations(['create_posts_table', 'create_faqs_table', 'modify_posts_columns'])
+            ->hasRoute('web')
+            ->hasCommand(migrateCommand::class)
+            ->hasTranslations();
+    }
+
+    protected function getResources(): array
+    {
+        // TagResouce should not be disabled.
+        return array_merge(
+            config('zeus-sky.enabled_resources'),
+            [TagResource::class]
+        );
+    }
+}
