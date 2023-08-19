b4c97231590e4ec238508052bbe75f132faeea45
Ash Monsh <wh7r.com@gmail.com>
2023-08-19 07:38:41 +0300

    add rain

---
+++ resources/views/themes/breeze/rain/widgets/FaqWidget.blade.php
@@ -0,0 +1,12 @@
+<div class="space-y-3">
+    @if($data['faqs'] !== null)
+        @foreach($data['faqs'] as $faq)
+            <x-filament::section class="my-4" collapsible :collapsed="$data['faqs']->count() >1">
+                <x-slot name="heading">
+                    <h3 class="font-semibold">{{ $faq->question }}</h3>
+                </x-slot>
+                {!! $faq->answer !!}
+            </x-filament::section>
+        @endforeach
+    @endif
+</div>
---
+++ resources/views/themes/daisyui/rain/widgets/LibraryWidget.blade.php
@@ -0,0 +1,31 @@
+<div>
+    @if($data['library'] !== null)
+        @foreach($data['library'] as $library)
+            <a href="{{ route('library.item', ['slug' => $library->slug]) }}" class="flex flex-col py-2 px-1.5 hover:bg-gray-100 dark:hover:bg-gray-600 rounded-md transition ease-in-out duration-500 block cursor-pointer">
+                <div x-data class="flex items-center justify-between text-primary-600 dark:text-primary-400 hover:dark:text-primary-300">
+                    <h3>{{ $library->title ?? '' }}</h3>
+                    @if($library->type === 'IMAGE')
+                        <span x-tooltip.raw="{{ __('Image') }}">
+                            @svg('heroicon-o-photo','w-4 h-4 text-gray-400 dark:text-gray-500')
+                        </span>
+                    @endif
+
+                    @if($library->type === 'FILE')
+                        <span x-tooltip.raw="{{ __('File') }}">
+                            @svg('heroicon-o-document','w-4 h-4 text-gray-400 dark:text-gray-500')
+                        </span>
+                    @endif
+
+                    @if($library->type === 'VIDEO')
+                        <span x-tooltip.raw="{{ __('Video') }}">
+                            @svg('heroicon-o-film','w-4 h-4 text-gray-400 dark:text-gray-500')
+                        </span>
+                    @endif
+                </div>
+                <cite class="text-sm text-custom-600 dark:text-custom-500 hover:dark:text-custom-300">
+                    {{ $library->description }}
+                </cite>
+            </a>
+        @endforeach
+    @endif
+</div>
